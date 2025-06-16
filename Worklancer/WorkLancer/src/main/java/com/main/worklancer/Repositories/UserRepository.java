74fcc974f3f2240c229445e8cd8618a5a2a3725b
HP <Nirbhavanepiyush109@gmail.com>
2025-06-16 22:52:50 +0530

    directory change

--- Worklancer/WorkLancer/src/main/java/com/main/worklancer/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.main.worklancer.Repositories;
-
-import com.main.worklancer.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
